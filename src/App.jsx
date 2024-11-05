405646ecdf9d2ec87692c75aca9d47f457937bec
SafiaKhalid <safia.am.khalid@gmail.com>
2024-11-05 16:05:05 +0000

    Added link in Home screen to AddRecipe screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './screens/Home';
+import AddRecipe from './screens/AddRecipe';
 
 const App = () => {
   return <Router>
     <Routes>
       <Route exact path='/' Component={Home}></Route>
+      <Route path='/add' Component={AddRecipe}></Route>
     </Routes>
   </Router>
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,19 +1,20 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 import { useGlobalContext } from "../context"
 
 const Home = () => {
     const { recipeList } = useGlobalContext()
- 
-    const addRecipeButton = () => {
-        console.log('add recipe');        
-    }
 
     return <main>
         <h1>Recipes</h1>
         {recipeList ? <h2>home, recipe valid</h2> : <section>
                 <h2>No recipes added!</h2>
-                <button onClick={addRecipeButton}>Add recipe</button>
+                <button>
+                    <Link to='/add'>
+                        Add recipe
+                    </Link>                    
+                </button>
             </section>}
     </main>    
 }
