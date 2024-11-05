a2ec7be6bc4daf4a6fb42a97851ca62caa07e942
SafiaKhalid <safia.am.khalid@gmail.com>
2024-11-05 16:13:36 +0000

    Started form in AddRecipe screen

--- src/screens/AddRecipe.jsx
+++ src/screens/AddRecipe.jsx
@@ -1,5 +1,28 @@
 const AddRecipe = () => {
-    return <h1>Add Recipe</h1>
+    return <main>
+        <h1>New Recipe</h1>
+
+        <form>
+            <div>
+                <label>Recipe name</label>
+                <input type="text" id="recipe_name"></input>
+            </div>
+            <div>
+                <label>Preparation time</label>
+                <input type="text" id="recipe_name"></input>
+            </div>
+            <div>
+                <label>Cook time</label>
+                <input type="text" id="recipe_name"></input>
+            </div>
+            <div>
+                <label>Servings</label>
+                <input type="text" id="recipe_name"></input>
+            </div>
+
+            <input type="submit" value="Add recipe" />
+        </form>
+    </main>
 }
 
 export default AddRecipe
