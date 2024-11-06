e818b4ac31698c91e01b13961547bdeb05fc3ebd
SafiaKhalid <safia.am.khalid@gmail.com>
2024-11-06 15:49:01 +0000

    Passed recipes to reducer using addRecipe function from context.jsx

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -1,14 +1,16 @@
 const defaultState = {
-    recipeList: JSON.parse(localStorage.getItem('recipeList'))
+    recipeList: JSON.parse(localStorage.getItem('recipeList')),
 }
 
 const reducer = (state, action) => {
     switch (action.type) {
         case 'ADD_RECIPE':
             // currently have to pass whole list, want to only have to pass added recipe            
-            localStorage.setItem('recipeList', JSON.stringify(action.payload))
+            /* localStorage.setItem('recipeList', JSON.stringify([...state.recipeList, action.payload])) */
+            /* console.log('payload:', action.payload); */
             
-            return {...state, recipeList: action.payload}
+            /* console.log('local storage:', localStorage); */
+            return { ...state, recipeList: action.payload }
         default:
             throw new Error('no matching type')
     }
