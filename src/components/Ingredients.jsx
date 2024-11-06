faf9aaa19dcb73dfdb671ef214fe5ef0ee8e2dda
SafiaKhalid <safia.am.khalid@gmail.com>
2024-11-06 12:44:56 +0000

    Added correct functionality to addIngredient button in Ingredients component

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
+
 const Ingredients = () => {
-    return <h1>ingredients</h1>
+    const [counter, setCounter] = useState(1)
+
+    const addIngredient = () => {
+        console.log('add ingredient');
+        setCounter(counter + 1)
+    }
+
+    return <fieldset>
+        <legend>Ingredients</legend>
+        <button onClick={addIngredient}>Add ingredient</button>        
+        {Array.from(Array(counter)).map((c, index) => {
+            return <input key={index} type="text" ></input>
+        })}
+    </fieldset>
 }
 
 export default Ingredients
