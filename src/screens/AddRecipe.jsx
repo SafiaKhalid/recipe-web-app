17f928e270987dea7cc7afbc85bd85646a655dc9
SafiaKhalid <safia.am.khalid@gmail.com>
2024-11-06 11:06:35 +0000

    Added requires to relevant inputs, added textarea tags

--- src/screens/AddRecipe.jsx
+++ src/screens/AddRecipe.jsx
@@ -10,14 +10,16 @@ const AddRecipe = () => {
         cookTime: null,
         servings: null,
         description: null,
-        ingredient: [],
-        method: [],
+        ingredients: [], //add
+        method: [], //add
         notes: null
     })
 
     const formSubmit = (e) => {
         e.preventDefault()
         console.log('submit')
+        console.log(e.target.recipe_name.value)
+        
     }
 
     return <main>
@@ -26,19 +28,31 @@ const AddRecipe = () => {
         <form onSubmit={formSubmit}>
             <div>
                 <label htmlFor='recipe_name'>Recipe name</label>
-                <input type="text" id="recipe_name"></input>
+                <input type="text" id="recipe_name" required></input>
+            </div>
+            <div>
+                <label htmlFor='category'>Category</label>
+                <input type="text" id="category" required></input>
             </div>
             <div>
                 <label htmlFor='prep_time'>Preparation time</label>
-                <input type="text" id="prep_time"></input>
+                <input type="text" id="prep_time" required></input>
             </div>
             <div>
                 <label htmlFor='cook_time'>Cook time</label>
-                <input type="text" id="cook_time"></input>
+                <input type="text" id="cook_time" required></input>
             </div>
             <div>
                 <label htmlFor='servings'>Servings</label>
                 <input type="text" id="servings"></input>
+            </div>
+            <div>
+                <label htmlFor='description'>Description</label>
+                <textarea id="description"></textarea>
+            </div>
+            <div>
+                <label htmlFor='notes'>Notes</label>
+                <textarea id="notes"></textarea>
             </div>
 
             <input type="submit" value="Add recipe" />
