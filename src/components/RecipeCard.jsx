7dad8e65b728a12503a21d6390c9a594b7544990
SafiaKhalid <safia.am.khalid@gmail.com>
2024-11-07 12:10:11 +0000

    Mapped RecipeCard component to Home screen

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,5 @@
+const RecipeCard = () => {
+    return <h2>recipe card</h2>
+}
+
+export default RecipeCard
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,7 @@
-import React from "react"
 import { Link } from "react-router-dom"
 
 import { useGlobalContext } from "../context"
+import RecipeCard from "../components/RecipeCard"
 
 const Home = () => {
     const { recipeList } = useGlobalContext()
@@ -9,12 +9,15 @@ const Home = () => {
     return <main>
         <h1>Recipes</h1>
         {recipeList ? <section>
-                <h2>home, recipe valid</h2>                
                 <button>
                     <Link to='/add'>
                         Add recipe
                     </Link>                    
                 </button>
+                <h2>home, recipe valid</h2>   
+                {recipeList.map((recipe) => {
+                    return <RecipeCard key={recipe.id} />
+                })}             
             </section>
              : <section>
                 <h2>No recipes added!</h2>
