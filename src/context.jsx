03ae29843f5854277292e7b5c8c1c60bdff4bdaa
SafiaKhalid <safia.am.khalid@gmail.com>
2024-11-13 15:18:58 +0000

    Log database when new data added in context.jsx

--- src/context.jsx
+++ src/context.jsx
@@ -2,26 +2,23 @@ import React, { useContext, useReducer } from "react";
 
 import { reducer, defaultState } from "./reducer";
 import { db } from "./assets/db";
+import { useLiveQuery } from "dexie-react-hooks";
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, defaultState)
+    const databaseList = useLiveQuery(() => db.recipies.toArray())
 
     const addRecipe = async (newRecipe) => {
-        /* let recipe
-        if (state.recipeList) {
-            recipe = [...state.recipeList, newRecipe]        
-        } else {
-            recipe = [newRecipe]
-        }
-         */
         console.log('New recipe in context.jsx: ', newRecipe);
         try {                
                 await db.recipies.add(newRecipe)
-            } catch (error) {
-                console.error(error)   
-            }
+        } catch (error) {
+            console.error(error)   
+        }            
+        
+        console.log('Database: ', databaseList);
         
         dispatch({type:'ADD_RECIPE', payload: newRecipe})
     }
