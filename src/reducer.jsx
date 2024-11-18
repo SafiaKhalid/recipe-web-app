3abe4bc15aaa114b2f6354d7bf5663c30c2b502c
SafiaKhalid <safia.am.khalid@gmail.com>
2024-11-18 12:24:48 +0000

    Added changeCurrentRecipe function to context.jsx, dispatch to reducer with currentRecipe as payload

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -18,7 +18,8 @@ const reducer = (state, action) => {
             /* console.log('local storage:', localStorage); */
             /* console.log('payload:', action.payload);      */         
             return { ...state, recipeList: [...(state.recipeList ? state.recipeList : []), action.payload] }
-            
+        /* case 'CHANGE_CURRENT': */
+
         default:
             throw new Error('no matching type')
     }
