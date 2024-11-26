aa3a0aa0bd84acc3e56a27f36dc45696d3b914dc
SafiaKhalid <safia.am.khalid@gmail.com>
2024-11-26 15:07:22 +0000

    Added select options to Home screen

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -7,14 +7,27 @@ const Home = () => {
     const { recipeList } = useGlobalContext()
     const recipeCopy = [...recipeList]
 
+
+    //find way to order recipeCopy based on user dropdown options for order
     return <main>
         <h1>Recipes</h1>
-        {recipeList ? <section>
+        {recipeCopy.length > 0 ? <section>
                 <button>
                     <Link to='/add'>
                         Add recipe
                     </Link>                    
                 </button>
+                <div>
+                    <label htmlFor="sort">Sort</label>
+                    <select name="sort" id="sort">
+                        <option value="dateDes">Date (newest)</option>
+                        <option value="dateAsc">Date (oldest)</option>
+                        <option value="aToZ">A - Z</option>
+                        <option value="zToA">Z - A</option>
+                        <option value="timeAsc">Time (ascending)</option>
+                        <option value="timeDes">Time (descending)</option>
+                    </select>
+                </div>
                 {recipeCopy.map((recipe) => {
                     return <RecipeCard key={recipe.id} recipe={recipe} />
                 })}             
